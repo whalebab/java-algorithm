cf8ab04854fd7f606bfb948dbe37c75276fa9e77
whalebab <juncoder94@gmail.com>
2024-05-23 22:15:41 +0900

    백준 Bronze 5 문제풀이

---
+++ src/Day2/BOJ10998.java
@@ -0,0 +1,14 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class BOJ10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A * B);
+    }
+}
