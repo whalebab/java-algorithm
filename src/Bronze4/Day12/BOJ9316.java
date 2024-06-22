55f731a4633c10a10e824036232fe051fe58a8d2
whalebab <juncoder94@gmail.com>
2024-06-22 22:08:50 +0900

    백준 코테 문제풀이

---
+++ src/Bronze4/Day12/BOJ9316.java
@@ -0,0 +1,14 @@
+package Bronze4.Day12;
+
+import java.util.Scanner;
+
+public class BOJ9316 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            System.out.println("Hello World, Judge " + (i+1) + "!");
+        }
+    }
+}
